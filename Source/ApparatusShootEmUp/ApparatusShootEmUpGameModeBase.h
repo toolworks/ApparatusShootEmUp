0c73044767c6b0440a0fc032485551c41249f901
Vladislav Dmitrievich Turbanov <vladislav@turbanov.ru>
2023-01-27 21:21:51 +0300

    * Initial project commit.

---
+++ Source/ApparatusShootEmUp/ApparatusShootEmUpGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ApparatusShootEmUpGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class APPARATUSSHOOTEMUP_API AApparatusShootEmUpGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
