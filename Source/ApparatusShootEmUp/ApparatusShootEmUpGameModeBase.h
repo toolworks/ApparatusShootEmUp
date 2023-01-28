ab12f9751cf013c565a8630c96f454394f8f7d75
Vladislav Dmitrievich Turbanov <vladislav@turbanov.ru>
2023-01-28 17:33:01 +0300

    * Main movement mechanic.

--- Source/ApparatusShootEmUp/ApparatusShootEmUpGameModeBase.h
+++ Source/ApparatusShootEmUp/ApparatusShootEmUpGameModeBase.h
@@ -4,14 +4,51 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/GameModeBase.h"
+
+#include "MechanicalGameModeBase.h"
+
+#include "Located.h"
+#include "Traits/Move.h"
+#include "Traits/PlayerTrait.h"
+#include "PlayerPawn.h"
+
 #include "ApparatusShootEmUpGameModeBase.generated.h"
 
 /**
- * 
+ * The main mechanical game mode.
  */
 UCLASS()
-class APPARATUSSHOOTEMUP_API AApparatusShootEmUpGameModeBase : public AGameModeBase
+class APPARATUSSHOOTEMUP_API AApparatusShootEmUpGameModeBase
+  : public AMechanicalGameModeBase
 {
 	GENERATED_BODY()
-	
+
+ protected:
+
+	void
+	SteadyTick(float DeltaTime) override
+	{
+		// Movement.
+		{
+			GetMechanism()->Operate(FFilter::Make<FMove, FLocated>(), [=](const FMove& Move, FLocated& Located)
+			{
+				Located.Location += Move.Velocity * DeltaTime;
+			});
+		}
+
+		// Location sync.
+		{
+			GetMechanism()->Operate([=](ISolidSubjective* Subjective, const FPlayerTrait& Player, const FLocated& Located)
+			{
+				Subjective->GetActor()->SetActorLocation(Located.Location);
+			});
+		}
+	}
+
+ public:
+
+	AApparatusShootEmUpGameModeBase()
+	{
+		DefaultPawnClass = APlayerPawn::StaticClass();
+	}
 };
