5ca95c7cc6906585e023c3bb407942075d23f040
Vladislav Dmitrievich Turbanov <vladislav@turbanov.ru>
2023-01-28 17:32:06 +0300

    * Main entity traits.

---
+++ Source/ApparatusShootEmUp/Traits/Enemy.h
@@ -0,0 +1,15 @@
+#pragma once
+ 
+#include "CoreMinimal.h"
+#include "Enemy.generated.h"
+ 
+/**
+ * The main enemy trait.
+ */
+USTRUCT(BlueprintType)
+struct APPARATUSSHOOTEMUP_API FEnemy
+{
+    GENERATED_BODY()
+ 
+  public:
+};
---
+++ Source/ApparatusShootEmUp/Traits/PlayerTrait.h
@@ -0,0 +1,15 @@
+#pragma once
+ 
+#include "CoreMinimal.h"
+#include "PlayerTrait.generated.h"
+ 
+/**
+ * The main player trait.
+ */
+USTRUCT(BlueprintType)
+struct APPARATUSSHOOTEMUP_API FPlayerTrait
+{
+	GENERATED_BODY()
+ 
+  public:
+};
