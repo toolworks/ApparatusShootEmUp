388eeb614ace43d9fd692e386ecac546a0bd01a6
Vladislav Dmitrievich Turbanov <vladislav@turbanov.ru>
2023-02-02 13:13:52 +0300

    * Enemies reach player and self-decouple.

--- Source/ApparatusShootEmUp/ApparatusShootEmUpGameModeBase.h
+++ Source/ApparatusShootEmUp/ApparatusShootEmUpGameModeBase.h
@@ -8,8 +8,10 @@
 #include "MechanicalGameModeBase.h"
 
 #include "Located.h"
+#include "BubbleCage.h"
 #include "Traits/Move.h"
 #include "Traits/PlayerTrait.h"
+#include "Traits/Enemy.h"
 #include "PlayerPawn.h"
 
 #include "ApparatusShootEmUpGameModeBase.generated.h"
@@ -25,9 +27,29 @@ class APPARATUSSHOOTEMUP_API AApparatusShootEmUpGameModeBase
 
  protected:
 
+	template < typename T, typename ...Ts >
+	FSubjectHandle
+	GetSingleton() const
+	{
+		const auto Filter = FFilter::Make<T, Ts...>();
+		const auto Chain = GetMechanism()->Enchain(Filter);
+	
+		auto Cursor = Chain->Iterate(0, 1);
+		if (!Cursor.Provide())
+		{
+			checkNoEntry();
+			return FSubjectHandle::Invalid;
+		}
+
+		auto SubjectHandle = Cursor.GetSubject();
+		verifyf(!Cursor.Provide(), TEXT("Two singleton objects detected!"));
+		return SubjectHandle;
+	}
+
 	void
 	SteadyTick(float DeltaTime) override
 	{
+		auto PlayerSubject = GetSingleton<FPlayerTrait>();
 		// Movement.
 		{
 			GetMechanism()->Operate(FFilter::Make<FMove, FLocated>(), [=](const FMove& Move, FLocated& Located)
@@ -36,12 +58,32 @@ class APPARATUSSHOOTEMUP_API AApparatusShootEmUpGameModeBase
 			});
 		}
 
+		// Decouple
+		{
+			ABubbleCage::Evaluate();
+		}
+
 		// Location sync.
 		{
 			GetMechanism()->Operate([=](ISolidSubjective* Subjective, const FPlayerTrait& Player, const FLocated& Located)
 			{
 				const auto Actor = CastChecked<APlayerPawn>(Subjective->GetActor());
 				Actor->SetActorLocation(Located.Location);
+			});
+		}
+
+		// Enemies reaching player.
+		{
+			const auto Target = PlayerSubject.GetTrait<FLocated>().GetLocation();
+			GetMechanism()->Operate<FUnsafeChain>([&](FUnsafeSubjectHandle Subject, const FEnemy& Enemy, const FLocated& Located)
+			{
+				const auto Delta = Target - Located.GetLocation();
+				const auto Direction = Delta.GetSafeNormal();
+				if (!Delta.IsZero())
+				{
+					auto& Move = Subject.ObtainTraitRef<FMove>();
+					Move.Velocity = Direction * 100;
+				}
 			});
 		}
 	}
