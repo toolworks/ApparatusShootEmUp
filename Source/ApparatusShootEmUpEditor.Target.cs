0e8747080f88e421ff056e52de0bd963c1864785
Vladislav Dmitrievich Turbanov <vturbanov@gmail.com>
2025-03-06 16:24:37 +0300

    * Compatiblity with UE-5.4

--- Source/ApparatusShootEmUpEditor.Target.cs
+++ Source/ApparatusShootEmUpEditor.Target.cs
@@ -8,7 +8,7 @@ public class ApparatusShootEmUpEditorTarget : TargetRules
 	public ApparatusShootEmUpEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
 
 		ExtraModuleNames.AddRange( new string[] { "ApparatusShootEmUp", "ApparatusEditor", "ApparatistEditor" } );
 	}
