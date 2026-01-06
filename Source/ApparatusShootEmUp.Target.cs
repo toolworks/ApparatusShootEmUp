a79f44cf73970330f8c66a4bf504db483fba7f3c
Vladislav Dmitrievich Turbanov <vturbanov@gmail.com>
2026-01-06 18:18:16 +0300

    Bumped up the UE version number.
    * The build settings version is also increased.

--- Source/ApparatusShootEmUp.Target.cs
+++ Source/ApparatusShootEmUp.Target.cs
@@ -8,7 +8,7 @@ public class ApparatusShootEmUpTarget : TargetRules
 	public ApparatusShootEmUpTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V5;
+		DefaultBuildSettings = BuildSettingsVersion.V6;
 
 		ExtraModuleNames.AddRange( new string[] { "ApparatusShootEmUp", "ApparatusRuntime", "ApparatistRuntime" } );
 	}
